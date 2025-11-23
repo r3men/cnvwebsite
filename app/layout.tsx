0f018e153c9b59c4eea6f47fc9005daa4b9c6d96
r3men <157990153+r3men@users.noreply.github.com>
2025-11-23 12:53:35 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Learn Cybersecurity",
+  title: "Code Name Verity",
   description: "Template from Vercel.",
   icons: {
     icon: "/favicon.png",
