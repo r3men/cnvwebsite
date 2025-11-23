74e8e62ea205aa9497063e7242e9295c27770e7d
r3men <157990153+r3men@users.noreply.github.com>
2025-11-23 14:12:42 -0500

    Update page.tsx

--- app/personal-review/page.tsx
+++ app/personal-review/page.tsx
@@ -11,7 +11,7 @@ export default function PersonalReview() {
             Personal Review
           </h1>
 
-          <p className="text-lg md:text-xl text-dossier-ink/80 opacity-0 animate-fadeIn animate-fadeIn-delay-100">
+          <p className="text-lg md:text-xl text-dossier-ink opacity-0 animate-fadeIn animate-fadeIn-delay-100">
             After reading this novel, I am beyond shocked. Originally, I found myself getting lost into the immense detail & many acronyms that were introduced during Julie’s confession towards the beginning of the novel. It was so intimidating for me since I was unfamiliar with most of these terms and I continued to forget what they meant even after looking them up, simply because there were so many to remember. However, upon reading further into the novel, specifically into the climax, I found myself completely hooked on the resilient friendship between Julie & Maddie. In addition to this, I enjoyed Wein’s use of language with German & French to provide a more historically accurate portrayal of the war. Wein’s intricate design of the plotline leading up to the eventual tragedy that unfolded later in the novel continues to baffle me, considering how well written & captivating it is. Overall, I will definitely recommend this novel to other historical fiction fans that are seeking a story of many twists, turns, & most prominently a powerful depiction of the importance of friendship.
           </p>
         </div>
